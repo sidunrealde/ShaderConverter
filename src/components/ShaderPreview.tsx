21f56959af5f9fcc9a8d9650d5f89f052cba3d7f
Siddartha <sid.gonnabattula@gmail.com>
2025-12-27 01:56:41 +0530

    Remove unused vertex shader attributes and uniforms
    Eliminated unused attribute and uniform declarations from the GLSL3 vertex shader string in ShaderPreview.tsx to clean up the shader code.

--- src/components/ShaderPreview.tsx
+++ src/components/ShaderPreview.tsx
@@ -13,14 +13,6 @@ interface ShaderPreviewProps {
 
 // Proper GLSL3 Vertex Shader
 const GLSL3_VERTEX = `
-in vec3 position;
-in vec3 normal;
-in vec2 uv;
-
-uniform mat4 modelViewMatrix;
-uniform mat4 projectionMatrix;
-uniform mat4 normalMatrix;
-
 out vec2 vUv;
 out vec3 vNormal;
 out vec3 vViewPosition;
